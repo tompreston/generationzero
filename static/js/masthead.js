921e9c13ae97394ff36868409dbcaaf0216101ad
kylehind <kyle@liquidbronze.com>
2016-02-15 19:27:01 +0000

    slide up masthead on scroll

--- static/js/masthead.js
+++ static/js/masthead.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
 	if(window.location.href == (window.location.origin + "/")){
-		setTimeout(function(){ 		
-			$("#masthead").slideUp( "slow", function(){
-				$("#branding").css("display", "inline-block");
-				$("#menu").css("display", "inline-block");
-			});
-		}, 1000);
+		$(window).scroll(function() {
+			var scrollPosition = $(window).scrollTop();
+			if (scrollPosition > 190){
+				$("#masthead").slideUp( "slow", function(){
+					$("#branding").css("display", "inline-block");
+					$("#menu").css("display", "inline-block");
+				});
+			}			
+		});		
 	} else {
 		$("#masthead").hide();
 		$("#branding").css("display", "inline-block");
