8b17e0d307dde610d746d0db74dbc2b00de42b00
Thomas Preston <thomasmarkpreston@gmail.com>
2016-05-11 00:28:14 +0100

    Added user to admin

--- magazine/admin.py
+++ magazine/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 from magazine.models import (Issue, Category, Entry)
 
 
 class MagazineAdminSite(admin.AdminSite):
 
     site_header = 'Magazine administration'
+
+
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = ('username',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'date_joined',
+                    'last_login',
+                    'is_superuser')
 
 
 class IssueAdmin(admin.ModelAdmin):
@@ -30,6 +43,8 @@ class EntryAdmin(admin.ModelAdmin):
 
 
 admin_site = MagazineAdminSite(name='magazine_admin')
+admin_site.register(User, UserAdmin)
+# admin_site.register(Group)
 admin_site.register(Issue, IssueAdmin)
 admin_site.register(Category, CategoryAdmin)
 admin_site.register(Entry, EntryAdmin)
