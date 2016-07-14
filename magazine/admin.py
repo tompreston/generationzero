40c62865d106b489cfc43392ed9ea60e753a9dcb
Thomas Preston <thomasmarkpreston@gmail.com>
2016-07-14 21:25:48 +0100

    Fixed Django user admin so that passwords can be changed

--- magazine/admin.py
+++ magazine/admin.py
@@ -2,22 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
 from magazine.models import (Issue, Category, Entry)
+from django.contrib.auth.admin import UserAdmin
 
 
 class MagazineAdminSite(admin.AdminSite):
 
     site_header = 'Magazine administration'
-
-
-class UserAdmin(admin.ModelAdmin):
-
-    list_display = ('username',
-                    'first_name',
-                    'last_name',
-                    'email',
-                    'date_joined',
-                    'last_login',
-                    'is_superuser')
 
 
 class IssueAdmin(admin.ModelAdmin):
@@ -42,9 +32,17 @@ class EntryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
+UserAdmin.list_display = ('username',
+                          'first_name',
+                          'last_name',
+                          'email',
+                          'date_joined',
+                          'last_login',
+                          'is_superuser')
+
+
 admin_site = MagazineAdminSite(name='magazine_admin')
 admin_site.register(User, UserAdmin)
-# admin_site.register(Group)
 admin_site.register(Issue, IssueAdmin)
 admin_site.register(Category, CategoryAdmin)
 admin_site.register(Entry, EntryAdmin)
