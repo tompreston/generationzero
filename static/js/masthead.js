d906a899b8f80e3aabc8dd0afd98b07278a75ae3
Thomas Preston <thomasmarkpreston@gmail.com>
2017-09-05 23:39:56 +0100

    Fix the masthead scrolling

--- static/js/masthead.js
+++ static/js/masthead.js
@@ -1,20 +1,51 @@
+var Masthead = {
+	show_masthead: true,
+	hidden: false,
+};
+
+/* hide the masthead */
+Masthead.hide = function() {
+	$("#masthead").slideUp({
+		duration: 'slow',
+		start: function(){
+			$("#branding").fadeIn();
+			/* prevent scrolling */
+			$('html, body').css({
+				overflow: 'hidden',
+				height: '100%'
+			});
+		},
+		done: function () {
+			/* reenable scrolling */
+			$('html, body').css({
+				overflow: 'auto',
+				height: 'auto'
+			});
+		}
+	});
+};
+
+Masthead.scroll = function() {
+	var scrollPosition = $(window).scrollTop();
+	if (scrollPosition > 2){
+		if (!Masthead.hidden) {
+			Masthead.hide();
+			Masthead.hidden = true;
+		}
+	}
+};
+
 $(document).ready(function(){
 	// if we're on the home page and not on a mobile device
-	if(window.location.href == (window.location.origin + "/") &&
-			$('#masthead').css('display') != 'none') {
-		$(window).scroll(function() {
-			var scrollPosition = $(window).scrollTop();
-			if (scrollPosition > 190){
-				$("#masthead").slideUp({
-					duration: 'slow',
-					start: function(){
-						$("#branding").fadeIn();
-					}
-				});
-			}
-		});
+	var show_masthead =
+		window.location.href == (window.location.origin + "/") &&
+		$('#masthead').css('display') != 'none';
+
+	if (show_masthead) {
+		$(window).scroll(Masthead.scroll);
 	} else {
 		$("#masthead").hide();
 		$("#branding").show();
 	}
 });
+
