3da51c13f74f398875a2fe9a188145067ee37434
Thomas Preston <thomasmarkpreston@gmail.com>
2018-02-22 21:52:55 +0000

    magazine: Add initial test

--- magazine/tests.py
+++ magazine/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+from django.urls import reverse
+
+
+class HomePageTests(TestCase):
+
+    def test_no_entries(self):
+        """
+        Test the page loads with no entries
+        """
+        response = self.client.get(reverse("home_page"))
+        self.assertEqual(response.status_code, 200)
+        """
+        print(response.content)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+        """
