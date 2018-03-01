89cfc79dfd5274854b17ac7f88c0763d0ddca501
Thomas Preston <thomasmarkpreston@gmail.com>
2018-03-01 22:21:48 +0000

    magazine: Add test for no entries and entries

--- magazine/tests.py
+++ magazine/tests.py
@@ -1,19 +1,35 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test import Client
 
 from django.urls import reverse
 
+from .models import Entry
+
 
 class HomePageTests(TestCase):
 
+    def setUp(self):
+        for i in range(50):
+            User.objects.create(username=f"fakeface{i}")
+
+    def add_entry(self, title, creator):
+        Entry.objects.create(title=title, created_by=creator)
+
     def test_no_entries(self):
-        """
-        Test the page loads with no entries
-        """
+        """Test the page loads with no entries"""
         response = self.client.get(reverse("home_page"))
         self.assertEqual(response.status_code, 200)
-        """
-        print(response.content)
-        self.assertContains(response, "No polls are available.")
-        self.assertQuerysetEqual(response.context['latest_question_list'], [])
-        """
+        self.assertNotContains(response, "grid-item")
+
+    def test_entires(self):
+        """Test the page loads with some entries"""
+        entries_per_user = 10
+        for user in User.objects.all():
+            for i in range(entries_per_user):
+                self.add_entry(f"An entry number {i} by {user.username}", user)
+
+        response = self.client.get(reverse("home_page"))
+        self.assertEqual(response.status_code, 200)
+        expected_entries = User.objects.count() * entries_per_user
+        self.assertContains(response, "grid-item", count=expected_entries)
